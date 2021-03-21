57b8d3faa4a377dddbc51e93e7325cc14cf5b4ee
oo0ili0oo <80237064+oo0ili0oo@users.noreply.github.com>
2021-03-21 10:37:48 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+#define n 10
+
+int main()
+{
+    int i;
+    double x, sum = 0, add = 1;
+
+    do
+    {
+        cout << "Input |x| < 1: ";
+        cin>>x;
+    }
+    while(fabs(x) >= 1);
+
+    for(i = 0; i <= n; add *= (i + 1) * x, ++i)
+        sum += add;
+
+    cout << "\nResult = %.2f\n" << sum;
+    return 0;
+}
